7747797df2faa1cfc2ad1cc8621cc179f5021c65
ignaciodv <nacho-unlz@hotmail.com>
2024-01-27 22:10:53 -0300

    Primera modificaion

---
+++ index.js
@@ -0,0 +1,26 @@
+
+function agregarItem(){
+    const nuevoItemUInput = document.getElementById('nuevoItem');
+    const nuevoItem = nuevoItemUInput.value;
+
+    const nuevoItemElemento = document.createElement('li');
+    nuevoItemElemento.textContent = nuevoItem;
+
+    nuevoItemElemento.addEventListener('click', function(){
+        this.classList.toggle('comprado')
+    })
+
+    document.getElementById('listaCompras').appendChild(nuevoItemElemento)
+
+    nuevoItemUInput.value = '';
+
+}
+
+function limpiarComprados(){
+    const lista = document.getElementById('listaCompras')
+    const elementoComprados = lista.querySelectorAll('.comprado')
+
+    elementoComprados.forEach(function(elemento){
+        lista.removeChild(elemento);
+    })
+}
